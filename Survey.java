59a9e379feeb8a39787010126a936844881b4805
zanettiprado <zanettiprado@gmail.com>
2024-02-22 02:48:15 +0000

    print next int and next double

--- Survey.java
+++ Survey.java
@@ -1,29 +1,39 @@
+import java.util.Scanner;
+
 public class Survey {
     public static void main(String[] args) {
 
         //*********PART A: PICKING UP THE USER'S ANSWERS*********
-        System.out.println("Welcome. Thank you for taking the survey");
+        System.out.println("\nWelcome. Thank you for taking the survey");
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
 
-        System.out.println("What is your name?");
-        String name;
+        System.out.println("\nWhat is your name?");
+        String name = scanner.nextLine();
+        counter++;
 
-        System.out.println("How much money do you spend on coffee?");
-        double coffeePrice;
+        System.out.println("\nHow much money do you spend on coffee?");
+        double coffeePrice = scanner.nextDouble();
+        counter++;
 
-        System.out.println("How much money do you spend on fast food?");
-        double foodPrice;
+        System.out.println("\nHow much money do you spend on fast food?");
+        double foodPrice = scanner.nextDouble();
+        counter++;
 
+        System.out.println("\nHow many times a week do you buy coffee?");
+        int coffeeAmount = scanner.nextInt();
+        counter++;
 
-        System.out.println("How many times a week do you buy coffee?");
-        int coffeeAmount;
+        System.out.println("\nHow many times a week do you buy fast food?");
+        int foodAmount = scanner.nextInt();
+        counter++;
 
-        System.out.println("How many times a week do you buy fast food?");
-        int foodAmount;
+        scanner.close();    
 
         //*********PART B: RESPONDING TO THE USER**********
-        System.out.println("Thank you <name> for answering all <counter> questions");
-        System.out.println("Weekly, you spend $<totalPrice> on coffee");
-        System.out.println("Weekly, you spend $<totalPrice> on food");
+        System.out.println("\nThank you, " + name + "! for answering all " + counter + " questions");
+        System.out.println("\nWeekly, you spend $" +(coffeeAmount * coffeePrice) + " on coffee");
+        System.out.println("\n Weekly, you spend $" + (foodAmount * foodPrice) +  " on food");
 
     }
 }
